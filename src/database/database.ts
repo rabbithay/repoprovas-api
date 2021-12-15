a04c32df596c2b9cb7028bb4e559887567358706
rabbithay <thay.csb@gmail.com>
2021-12-15 19:29:05 -0300

    fix: type type

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,8 +6,8 @@ dotenv.config();
 export default async function connect () {
   const connectionManager = await getConnectionManager();
   const connection = connectionManager.create({
-    name: process.env.ORM_NAME,
-    type: process.env.ORM_TYPE,
+    name: "default",
+    type: "postgres",
     url: process.env.ORM_URL,
     entities: ["src/entities/*.ts"]
   });
