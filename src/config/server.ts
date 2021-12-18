01aaa928eaf4ee80f525443cb9b2d0f112afe459
rabbithay <thay.csb@gmail.com>
2021-12-18 20:23:53 -0300

    refactor: delete files

--- src/config/server.ts
+++
@@ -1,9 +0,0 @@
-import app, { init } from '../app';
-
-const port = 4002;
-
-init().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-});
--- src/controllers/example.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-import * as exampleService from '../services/example';
-
-
-export async function example1(req: Request, res: Response) {
-  //
-}
-
