f5e2e9d29d040ef15c86b2a9dba8e2b437f26b9a
rabbithay <thay.csb@gmail.com>
2021-12-20 15:55:15 -0300

    feat: update subject route

--- src/controllers/subjects.ts
+++ src/controllers/subjects.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from 'express';
 import * as subjectsService from '@/services/subjects'
 
-export async function getSubjects(req: Request, res: Response) {
-  const subjects = await subjectsService.getSubjects();
+export async function getSubjectsAndTeachers(req: Request, res: Response) {
+  const subjects = await subjectsService.getSubjectsAndTeachers();
   res.status(200).send(subjects);
 }
 
-export async function getSubjectById(req: Request, res: Response) {
-  const id = req.params.id
-  const subject = await subjectsService.getSubjectById(Number(id))
-  console.log({subject})
+export async function getSubjectsAndExams(req: Request, res: Response) {
+  const subject = await subjectsService.getSubjectsAndExams()
   return res.status(200).send(subject);
 }
--- src/services/subjects.ts
+++ src/services/subjects.ts
@@ -1,13 +1,12 @@
 import { getRepository } from "typeorm";
 import Subject from '@/entities/Subject'
 
-export async function getSubjects() {
-  const subjects = await getRepository(Subject).find({relations:["teachers", "exams"]});
+export async function getSubjectsAndTeachers() {
+  const subjects = await getRepository(Subject).find({relations:["teachers"]});
   return subjects;
 }
 
-
-export async function getSubjectById(subjectId: number){
-  const subject = await getRepository(Subject).find({ id: subjectId });
-  return subject;
+export async function getSubjectsAndExams(){
+  const subjects = await getRepository(Subject).createQueryBuilder("subjects").innerJoinAndSelect("subjects.exams", "exams").innerJoinAndSelect("exams.teacher", "teacher").getMany() 
+  return subjects;
 }
