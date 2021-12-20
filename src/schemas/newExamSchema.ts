6c9b5907eb988ba1a0d523437febdf9e131c41e3
rabbithay <thay.csb@gmail.com>
2021-12-20 16:14:52 -0300

    fix: new exam schema types

--- src/schemas/newExamSchema.ts
+++ src/schemas/newExamSchema.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const newExamSchema = Joi.object({
     title: Joi.string().required(),
     category: Joi.string().valid('P1', 'P2', 'P3', '2ch', 'Outras').required(),
-    subject: Joi.string().required(),
-    teacher: Joi.string().required(),
+    subjectId: Joi.number().required(),
+    teacherId: Joi.number().required(),
     link: Joi.string().uri().required()
 });
