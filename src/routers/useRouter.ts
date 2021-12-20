9d36d67ca5d98dd9be1c82bbf5e7f803b91c9be7
rabbithay <thay.csb@gmail.com>
2021-12-19 23:39:12 -0300

    feat: create exams routes

--- src/routers/useRouter.ts
+++ src/routers/useRouter.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import * as subjectsController from '@/controllers/subjects'
+import * as examsController from '@/controllers/exams'
+import * as teachersController from '@/controllers/teachers'
 
 const router = Router();
 
@@ -7,16 +9,16 @@ router.get('/health', (req, res) => {
     res.status(200).send("OK!")
 });
 
-router.post('/exams', () => {});
+router.post('/exams', examsController.postExam);
 
-router.get('/exams', () => {});
+router.get('/exams', examsController.getExams);
 
 router.get('/subjects', subjectsController.getSubjects);
 
-router.get('/subjects/:id', () => {});
+router.get('/subjects/:id', subjectsController.getSubjectById);
 
-router.get('/teachers', () => {});
+router.get('/teachers', teachersController.getTeachers);
 
-router.get('/teachers/:id', () => {});
+router.get('/teachers/:id',teachersController.getTeacherById);
 
 export default router;
