7792b2c88e5f25c3792d4c3cff71adcc528a9d65
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-01-04 10:19:12 -0300

    prepara revisão

--- src/services/utils.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export function validateObject({
-  object, schema,
-}: {object: object, schema: Joi.ObjectSchema<any>}) {
-  const validation = schema.validate(object);
-  console.log(validation.error)
-  return !validation.error;
-}
